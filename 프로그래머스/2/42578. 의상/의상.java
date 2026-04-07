b140602c2c4c33cdc31f9015ea026c6af795a88b
YunTaeng195 <howard10322@gmail.com>
2026-04-07 13:43:59 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String[] cloth : clothes) {
+            String type = cloth[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
+        int answer=1;
+        
+        for(int cnt : map.values()){
+            answer*=(cnt+1);
+        }
+        
+        
+        
+        
+        return answer-1;
+    }
+}
